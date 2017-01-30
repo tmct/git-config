b6dd11aec4825b08cffe4ddb72afabc42534ed04
Tom McTiernan <tmct@users.noreply.github.com>
2017-01-30 09:51:41 +0000

    Create config.sh

---
+++ config.sh
@@ -0,0 +1,13 @@
+git config --global alias.cp 'cherry-pick'
+git config --global alias.st 'status -sb'
+git config --global alias.cl 'clone'
+git config --global alias.ci 'commit'
+git config --global alias.co 'checkout'
+git config --global alias.br 'branch'
+git config --global alias.dc 'diff --cached'
+git config --global alias.lg "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %Cblue<%an>%Creset' --abbrev-commit --date=relative --all"
+git config --global alias.last 'log -1 --stat'
+git config --global alias.unstage 'reset HEAD --'
+git config --global alias.boom 'reset --hard HEAD'
+
+# From [babun](https://github.com/babun/babun/blob/991159167bc4b3c2d1c3fb4aaae12d7710f45504/babun-core/plugins/git/install_home.sh)
