a4acf2d01d498f183fa1235a6af28d10bb1789f3
Tom McTiernan <tmct@users.noreply.github.com>
2017-02-15 18:08:10 +0000

    Git log but not all

--- config.sh
+++ config.sh
@@ -5,7 +5,8 @@ git config --global alias.ci 'commit'
 git config --global alias.co 'checkout'
 git config --global alias.br 'branch'
 git config --global alias.dc 'diff --cached'
-git config --global alias.lg "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %Cblue<%an>%Creset' --abbrev-commit --date=relative --all"
+git config --global alias.lg "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %Cblue<%an>%Creset' --abbrev-commit --date=relative"
+git config --global alias.la "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %Cblue<%an>%Creset' --abbrev-commit --date=relative --all"
 git config --global alias.last 'log -1 --stat'
 git config --global alias.unstage 'reset HEAD --'
 git config --global alias.boom 'reset --hard HEAD'
